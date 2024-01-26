b0b2374c90238134eb45922d9775d5999fc113b7
J-Skarbek <john@snarkmediaagency.com>
2024-01-26 14:08:31 -0600

    Fix issue in author_list view

--- application/controllers/authorController.js
+++ application/controllers/authorController.js
@@ -1,4 +1,5 @@
 const Author = require('../models/author');
+const Book = require("../models/book");
 const asyncHandler = require('express-async-handler');
 
 //Display list of authors
@@ -12,8 +13,27 @@ exports.author_list = asyncHandler(async (req, res, next) => {
 })
 
 //Display detail page for specific Author
-exports.author_detail = asyncHandler(async (req, rest, next) => {
-  res.send(`NOT IMPLEMENTED: Author Detail: ${req.params.id}`);
+exports.author_detail = asyncHandler(async (req, res, next) => {
+  // Get details of author and all their books (in parallel)
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (author === null) {
+    // No results.
+    const err = new Error("Author not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("author_detail", {
+    title: "Author Detail",
+    author: author,
+    author_books: allBooksByAuthor,
+  });
+
+  // res.send(`NOT IMPLEMENTED: Author Detail: ${req.params.id}`);
 })
 
 //Display Author create form on a GET
